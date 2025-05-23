4642b11f3dc4f091d128bb6f782850b7fd236300
Nuria Mohamed Hussein <nuria.hussein@student.moringaschool.com>
2025-05-23 21:13:06 +0300

    commit Getting Remote Data lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,17 @@
 import requests
 import json
 
-class GetRequester:
 
+class GetRequester:
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+
+        response = requests.get(self.url)
+        response.raise_for_status()
+        return response.content
 
     def load_json(self):
-        pass
+        response_text = self.get_response_body().decode('utf-8')
+        return json.loads(response_text)
