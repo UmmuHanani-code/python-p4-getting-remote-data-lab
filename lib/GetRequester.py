f0ac8f72ed57e6e45dcd08708fcb2529c44d28b8
Nuria Mohamed Hussein <nuria.hussein@student.moringaschool.com>
2025-05-23 21:17:06 +0300

    commit update

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -15,3 +15,6 @@ class GetRequester:
     def load_json(self):
         response_text = self.get_response_body().decode('utf-8')
         return json.loads(response_text)
+
+get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+print(get_requester.load_json())
